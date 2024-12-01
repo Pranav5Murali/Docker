dd003da559084f1fe5d712ede8346f20ac64e392
Pranav5Murali <154644683+Pranav5Murali@users.noreply.github.com>
2024-12-01 08:26:45 +0530

    Update create_python_container.py

--- create_python_container.py
+++ create_python_container.py
@@ -7,6 +7,7 @@ client = docker.from_env()
 image_name = "jitesoft/python"
 container_name = "jitesoft-python-container"
 network_name = "surge"  # Custom bridge network
+app_command = "python /app/app.py"  # Command to run the Python application
 
 # Stop and remove existing container if it exists
 try:
@@ -17,18 +18,19 @@ try:
 except docker.errors.NotFound:
     print("No existing container named '%s' found." % container_name)
 
-# Pull the pre-built image from Docker Hub
+# Pull the base image (jitesoft/python)
 print("Pulling Docker image '%s' from Docker Hub..." % image_name)
 client.images.pull(image_name)
 
-# Run the container
-print("Running Docker container '%s' on network '%s'..." % (container_name, network_name))
+# Run the container with the command to execute app.py
+print("Running Docker container '%s' on network '%s' with command '%s'..." % (container_name, network_name, app_command))
 client.containers.run(
     image=image_name,
     name=container_name,
     network=network_name,  # Attach to the custom bridge network
     ports={"5000/tcp": 5000},  # Map port 5000
-    detach=True
+    detach=True,
+    command=app_command  # Run the app.py script inside the container
 )
 
 print("The container '%s' is running successfully on the '%s' network!" % (container_name, network_name))
